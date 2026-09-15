c26ac724f0374302396fa6c58c09410b943718d0
itzjb <gogror0987@gmail.com>
2026-09-15 15:05:05 +0900

    Computer

---
+++ ex05/src/main/java/itzjb/ex05/computer/Computer.java
@@ -0,0 +1,7 @@
+package itzjb.ex05.computer;
+
+public class Computer {
+    public void turnOn() {
+        System.out.println("컴퓨터를 켜요");
+    }
+}
---
+++ ex05/src/main/java/itzjb/ex05/computer/MacBook.java
@@ -0,0 +1,10 @@
+package itzjb.ex05.computer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MacBook {
+    public void turnOn() {
+        System.out.println("맥북을 켜요");
+    }
+}
