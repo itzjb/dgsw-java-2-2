43ca20477bbb633e44be61375cffe8826a81d1a1
itzjb <gogror0987@gmail.com>
2026-09-15 15:05:36 +0900

    Data

---
+++ ex06/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into member (email, name) values ('1@email.com', 'name1');
+insert into member (email, name) values ('2@email.com', 'name2');
+insert into member (email, name) values ('3@email.com', 'name3');
+insert into member (email, name) values ('4@email.com', 'name4');
+
+insert into product (name, price) values ('product1', 1000);
+insert into product (name, price) values ('product2', 2000);
+insert into product (name, price) values ('product3', 3000);
